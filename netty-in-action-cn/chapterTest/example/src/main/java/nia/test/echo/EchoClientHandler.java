bf2e9652b141bb42e7171e31ea45c3d087f93d67
inzahgi <inzahgi@126.com>
2018-04-12 23:11:25 +0800

    add echo

--- netty-in-action-cn/chapterTest/example/src/main/java/nia/test/echo/EchoClientHandler.java
+++ netty-in-action-cn/chapterTest/example/src/main/java/nia/test/echo/EchoClientHandler.java
@@ -1,4 +1,39 @@
 package nia.test.echo;
 
-public class EchoClientHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+    private ByteBuf firstmessage;
+
+    public EchoClientHandler(){
+        firstmessage = Unpooled.buffer(EchoClient.SIZE);
+        for (int i = 0; i < firstmessage.capacity(); i++) {
+            firstmessage.writeByte((byte)i);
+        }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(firstmessage);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ctx.write(msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
