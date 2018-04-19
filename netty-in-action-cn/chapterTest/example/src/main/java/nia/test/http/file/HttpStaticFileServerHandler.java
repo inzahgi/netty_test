c9172402fafb3ce9a4439b36614b0bde1c3a4bc2
inzahgi <inzahgi@126.com>
2018-04-19 23:23:32 +0800

    fix http file

--- netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/file/HttpStaticFileServerHandler.java
+++ netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/file/HttpStaticFileServerHandler.java
@@ -1,8 +1,27 @@
 package nia.test.http.file;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
+import io.netty.util.internal.SystemPropertyUtil;
+
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.regex.Pattern;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.FOUND;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
@@ -11,7 +30,114 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
     public static final int HTTP_CACHE_SECONDS = 60;
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
+        if(!request.decoderResult().isSuccess()){
+
+        }
 
     }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        if(ctx.channel().isActive()){
+            sendError(ctx.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    private static final Pattern INSECURE_URI = Pattern.compile(".*[<>&\"].*]");
+
+    private static String sanitizeUri(String uri){
+        try{
+            uri = URLDecoder.decode(uri, "UTF-8");
+        }catch (UnsupportedEncodingException e){
+            throw new Error(e);
+        }
+
+        if(uri.isEmpty() || uri.charAt(0) != '/'){
+            return null;
+        }
+
+        uri = uri.replace('/', File.separatorChar);
+
+        if(uri.contains(File.separator + '.') ||
+                uri.contains('.' + File.separator) ||
+                uri.charAt(0) == '.' ||
+                uri.charAt(uri.length()-1) == '.' ||
+                INSECURE_URI.matcher(uri).matches()){
+            return null;
+        }
+
+        return SystemPropertyUtil.get("user.dir") + File.separator + uri;
+    }
+
+    private static final Pattern ALLOWED_FILE_NAME = Pattern.compile("[^-\\._]?[^<>&\\\"]*]");
+
+    private static void sendListing(ChannelHandlerContext ctx, File dir, String dirPath){
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, OK);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html;charset=UTF-8");
+
+        StringBuilder buf = new StringBuilder()
+                .append("<!DOCTYPE html>\r\n")
+                .append("<html><>head><meta charset='utf-8' /><title>")
+                .append("Listing of:")
+                .append(dirPath)
+                .append("</title></head><body>\r\n")
+
+                .append("<h3>Listing of:")
+                .append("dirPath")
+                .append("</h3>\r\n")
+
+                .append("<ul>")
+                .append("<li><a href=\"../\">..</a></li>\r\n");
+        for(File f: dir.listFiles()){
+            if(f.isHidden() || !f.canRead()){
+                continue;
+            }
+
+            String name = f.getName();
+            if(!ALLOWED_FILE_NAME.matcher(name).matches()){
+                continue;
+            }
+
+            buf.append("<li><a href=\"")
+                    .append(name)
+                    .append("\">")
+                    .append(name)
+                    .append("</a></li>\r\n");
+
+        }
+
+        buf.append("</ul></body></html>\r\n");
+        ByteBuf buffer = Unpooled.copiedBuffer(buf, CharsetUtil.UTF_8);
+        response.content().writeBytes(buffer);
+        buffer.release();
+
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+    }
+
+    private static void sendRedirect(ChannelHandlerContext ctx, String newUri){
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, FOUND);
+        response.headers().set(HttpHeaderNames.LOCATION, newUri);
+
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+
+    }
+
+    private static void
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
