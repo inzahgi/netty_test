3ffa400986a71d543b3cacff280341cbbb7f75f0
inzahgi <inzahgi@126.com>
2018-04-20 23:10:05 +0800

    fix http file

--- netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/file/HttpStaticFileServerHandler.java
+++ netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/file/HttpStaticFileServerHandler.java
@@ -2,26 +2,49 @@ package nia.test.http.file;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelProgressiveFuture;
+import io.netty.channel.ChannelProgressiveFutureListener;
+import io.netty.channel.DefaultFileRegion;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.DefaultHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpChunkedInput;
 import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.ssl.SslHandler;
+import io.netty.handler.stream.ChunkedFile;
 import io.netty.util.CharsetUtil;
 import io.netty.util.internal.SystemPropertyUtil;
 
+import javax.activation.MimetypesFileTypeMap;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 import java.util.regex.Pattern;
 
-import static io.netty.handler.codec.http.HttpResponseStatus.FOUND;
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpMethod.GET;
+import static io.netty.handler.codec.http.HttpResponseStatus.*;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
@@ -32,7 +55,102 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
     @Override
     public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) throws Exception {
         if(!request.decoderResult().isSuccess()){
+            sendError(ctx,BAD_REQUEST);
+            return;
+        }
+        if(request.method() != GET){
+            sendError(ctx, METHOD_NOT_ALLOWED);
+            return;
+        }
 
+        final String uri = request.uri();
+        final String path = sanitizeUri(uri);
+        if(path == null){
+            sendError(ctx, FORBIDDEN);
+            return;
+        }
+
+        File file = new File(path);
+        if(file.isHidden() || !file.exists()){
+            sendError(ctx, NOT_FOUND);
+            return;
+        }
+
+        if(file.isDirectory()){
+            if(uri.endsWith("/")){
+                sendListing(ctx, file, uri);
+            }else {
+                sendRedirect(ctx, uri + '/');
+            }
+            return;
+        }
+
+        if(!file.isFile()){
+            sendError(ctx, FORBIDDEN);
+            return;
+        }
+
+        String ifModifiedSince = request.headers().get(HttpHeaderNames.IF_MODIFIED_SINCE);
+        if(ifModifiedSince != null && !ifModifiedSince.isEmpty()){
+            SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+            Date ifModifiedSinceDate = dateFormatter.parse(ifModifiedSince);
+
+            long ifModifiedSinceDateSeconds = ifModifiedSinceDate.getTime() / 1000;
+            long fileLastModifiedSeconds = file.lastModified() / 1000;
+            if(ifModifiedSinceDateSeconds == fileLastModifiedSeconds){
+                sendNotModified(ctx);
+                return;
+            }
+        }
+
+        RandomAccessFile raf;
+        try{
+            raf = new RandomAccessFile(file, "r");
+        }catch (FileNotFoundException ignore){
+            sendError(ctx, NOT_FOUND);
+            return;
+        }
+        long fileLength = raf.length();
+
+        HttpResponse response = new DefaultHttpResponse(HTTP_1_1, OK);
+        HttpUtil.setContentLength(response, fileLength);
+        setContentTypeHeader(response, file);
+        setDateAndCacheHeaders(response, file);
+        if(HttpUtil.isKeepAlive(request)){
+            response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+        }
+        ctx.write(response);
+
+        ChannelFuture sendFileFuture;
+        ChannelFuture lastContentFuture;
+        if(ctx.pipeline().get(SslHandler.class) == null){
+            sendFileFuture =
+                    ctx.write(new DefaultFileRegion(raf.getChannel(), 0, fileLength), ctx.newPromise());
+            lastContentFuture = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+        }else{
+            sendFileFuture =
+                    ctx.writeAndFlush(new HttpChunkedInput(new ChunkedFile(raf, 0, fileLength, 8192)));
+            lastContentFuture = sendFileFuture;
+        }
+
+        sendFileFuture.addListener(new ChannelProgressiveFutureListener(){
+            @Override
+            public void operationProgressed(ChannelProgressiveFuture future, long progress, long total) throws Exception {
+                if(total < 0) {
+                    System.err.println(future.channel() + " Transfer progress: " + progress);
+                }else {
+                    System.err.println(future.channel() + " Transfer progress: " + progress + " / " + total);
+                }
+            }
+
+            @Override
+            public void operationComplete(ChannelProgressiveFuture future) throws Exception {
+                System.err.println(future.channel() + " Transfer complete.");
+            }
+        });
+
+        if(!HttpUtil.isKeepAlive(request)){
+            lastContentFuture.addListener(ChannelFutureListener.CLOSE);
         }
 
     }
@@ -41,7 +159,7 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
         if(ctx.channel().isActive()){
-            sendError(ctx.INTERNAL_SERVER_ERROR);
+            sendError(ctx, INTERNAL_SERVER_ERROR);
         }
     }
 
@@ -74,7 +192,7 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
     private static final Pattern ALLOWED_FILE_NAME = Pattern.compile("[^-\\._]?[^<>&\\\"]*]");
 
     private static void sendListing(ChannelHandlerContext ctx, File dir, String dirPath){
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, OK);
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK);
         response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html;charset=UTF-8");
 
         StringBuilder buf = new StringBuilder()
@@ -118,14 +236,69 @@ public class HttpStaticFileServerHandler extends SimpleChannelInboundHandler<Ful
     }
 
     private static void sendRedirect(ChannelHandlerContext ctx, String newUri){
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, FOUND);
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, FOUND);
         response.headers().set(HttpHeaderNames.LOCATION, newUri);
 
         ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
 
     }
 
-    private static void
+    private static void sendError(ChannelHandlerContext ctx, HttpResponseStatus status){
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HTTP_1_1, status, Unpooled.copiedBuffer("Failure:" + status + "\r\n",
+                                                                    CharsetUtil.UTF_8));
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void sendNotModified(ChannelHandlerContext ctx){
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, NOT_MODIFIED);
+        setDateHeader(response);
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    private static void setDateHeader(FullHttpResponse response){
+        SimpleDateFormat dateFormat = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+        dateFormat.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
+
+        Calendar time = new GregorianCalendar();
+        response.headers().set(HttpHeaderNames.DATE, dateFormat.format(time.getTime()));
+
+    }
+
+    private static void setDateAndCacheHeaders(HttpResponse response, File fileToCache){
+        SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+        dateFormatter.setTimeZone(TimeZone.getTimeZone(HTTP_DATE_GMT_TIMEZONE));
+
+        Calendar time = new GregorianCalendar();
+        response.headers().set(HttpHeaderNames.DATE, dateFormatter.format(time.getTime()));
+
+        time.add(Calendar.SECOND, HTTP_CACHE_SECONDS);
+        response.headers().set(HttpHeaderNames.EXPIRES, dateFormatter.format(time.getTime()));
+        response.headers().set(HttpHeaderNames.CACHE_CONTROL, "private, max-age=" + HTTP_CACHE_SECONDS);
+        response.headers().set(
+                HttpHeaderNames.LAST_MODIFIED, dateFormatter.format(new Date(fileToCache.lastModified()))
+        );
+
+
+    }
+
+    private static void setContentTypeHeader(HttpResponse response, File file){
+        MimetypesFileTypeMap mimeTypesMap = new MimetypesFileTypeMap();
+        response.headers()
+                .set(HttpHeaderNames.CONTENT_TYPE, mimeTypesMap.getContentType(file.getPath()));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
