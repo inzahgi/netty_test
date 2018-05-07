5a1cecd389b137c9211b972973f5ac1419af2631
inzahgi <inzahgi@126.com>
2018-05-07 23:56:34 +0800

    add

--- netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/websocketx/client/WebSocketClientHandler.java
+++ netty-in-action-cn/chapterTest/example/src/main/java/nia/test/http/websocketx/client/WebSocketClientHandler.java
@@ -1,4 +1,20 @@
 package nia.test.http.websocketx.client;
 
-public class WebSocketClientHandler {
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelPromise;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+
+public class WebSocketClientHandler extends SimpleChannelInboundHandler<Object> {
+
+    private final WebSocketClientHandshaker handshaker;
+    private ChannelPromise handshakerFuture;
+
+    public WebSocketClientHandler(WebSocketClientHandshaker handshaker){
+        this.handshaker = handshaker;
+    }
+
+    public ChannelFuture handshakeFuture(){
+        return handshakerFuture;
+    }
 }
